11efebb4fae983eea15c4b2910be2e4b6181adf0
Eva Schicker <64033870+evaschicker@users.noreply.github.com>
2022-04-12 16:18:18 -0400

    First Upload Rainer Chopper
    First upload 04-12-22

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('player'),
+    renderer: 'svg',
+    loop: true,
+    
+    // Name of .json file below:
+    path: 'data.json'
+
+})
